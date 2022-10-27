6b890c76153678935b675f4436d8bfb62c70bb4e
Dovudhon <dovudkhonhusanov5007@gmail.com>
2022-10-27 17:54:13 +0500

    bakalavr diplom

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,4 @@
 import React, {createContext, useEffect, useRef, useState} from "react";
-import {json} from "react-router-dom";
 
 const initialState = {
     productInCartNum: JSON.parse(localStorage.getItem("productNum")) || [],
@@ -11,7 +10,6 @@ export const Context = createContext(initialState)
 export const ContextProvider = ({children}) => {
     const [productInCart, setProductInCart] = useState(initialState.productInCartNum)
     const [dataInCart, setDataInCart] = useState([initialState.dataCart])
-    const saveProduct = (product) => setDataInCart([...dataInCart, dataInCart.push(localStorage.setItem('product', JSON.stringify(product)))])
     const ref = useRef()
     const cartRef = (e) => {
         e.stopPropagation()
@@ -21,19 +19,8 @@ export const ContextProvider = ({children}) => {
     const removeRef = () => {
         ref.current.classList.remove("active")
     }
-
-    const addToCart = (product) => {
-<<<<<<< HEAD
-        products = [...products, product]
-        saveProduct(products)
-        window.location.reload()
-=======
-        saveProduct(product)
->>>>>>> 39b4aa562c5a0bdb17d7d24d36ff5f9455be315a
-    }
-
     const value = {
-        productInCart, setProductInCart, dataInCart, setDataInCart, cartRef, ref, removeRef, addToCart
+        productInCart, setProductInCart, dataInCart, setDataInCart, cartRef, ref, removeRef
     }
 
     return (
